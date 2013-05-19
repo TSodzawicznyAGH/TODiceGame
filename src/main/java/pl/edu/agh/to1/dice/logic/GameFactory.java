d01210e1c3b758e9d6613e8f356665878ea64362
johnnyr7 <resiak.jan@gmail.com>
2013-05-19 22:13:51 +0200

    max 4 players :)

--- src/main/java/pl/edu/agh/to1/dice/logic/GameFactory.java
+++ src/main/java/pl/edu/agh/to1/dice/logic/GameFactory.java
@@ -2,7 +2,7 @@ package pl.edu.agh.to1.dice.logic;
 
 public class GameFactory {
     static Game getStdGame() {
-        return new Game(new GameState(), 2, 3, 2);
+        return new Game(new GameState(), 2, 4, 2);
     }
     static Game getTriGame() {
         return new Game(new TriGameState(), 2, 3, 2);
